6b8323e80b78c39c053e9067e5823192ad8deea7
Dave Smits <dave@familie-smits.com>
2018-06-25 16:54:22 +0200

    cleaning up some code

--- samples/Custom/CustomCoreReceiver/CustomWebHook/PandoraConstants.cs
+++ samples/Custom/CustomCoreReceiver/CustomWebHook/PandoraConstants.cs
@@ -1,18 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace P112.WebHooks
 {
-    public static class PandoraConstants
+    internal static class PandoraConstants
     {
         public static string ReceiverName => "pandora";
 
         public static string Challenge = "echo";
-
-        public static string SignatureHeaderName => "ms-signature";
-
-        public static int SecretKeyMinLength => 32;
-        public static int SecretKeyMaxLength => 64;
     }
 }
